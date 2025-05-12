f550a966b67981398cdc0cb8f7a8afb7f266980a
CodeFrenzyOfficial <teamcodefrenzy@gmail.com>
2025-05-13 01:43:29 +0500

    changes in modules

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const dotenv = require('dotenv');
 const cors = require('cors');
 const firebase_routes = require("./routes/firebase");
-const http =  require("http");
-const { Server } = require("socket.io");
+// const http =  require("http");
+// const { Server } = require("socket.io");
 
 dotenv.config();
 const app = express();
@@ -12,20 +12,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // socket
-const server = http.createServer(app);
-const socket = new Server(server, {
-  cors: {
-    origin: "*"
-  }
-});
+// const server = http.createServer(app);
+// const socket = new Server(server, {
+//   cors: {
+//     origin: "*"
+//   }
+// });
 
-socket.on('connection', (client) => {
-  console.log('socket connected');
+// socket.on('connection', (client) => {
+//   console.log('socket connected');
   
-  client.on('firebase-create', (data) => {
-    console.log("Connected Users", data);
-  })
-})
+//   client.on('firebase-create', (data) => {
+//     console.log("Connected Users", data);
+//   })
+// })
 
 // server.listen(3002, () =>
 //   console.log(`Socket running on port: 3002`)
